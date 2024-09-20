ade472c6fdefab2f83bdb3b353930e8cd9eda1df
njmeister <150966120+njmeister@users.noreply.github.com>
2024-09-20 11:56:21 -0500

    addressing deploy error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	  plugins: [react(), svgr({
-		svgrOptions: {
-			icon: true,
-		},
-	  })],
+  plugins: [react(), svgr({
+    svgrOptions: {
+      icon: true,
+    },
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src'),
+    },
+  },
   server: {
-	port: 3000,
+    port: 3000,
   },
   build: {
-	outDir: 'dist',
+    outDir: 'dist',
   },
-})
+});
